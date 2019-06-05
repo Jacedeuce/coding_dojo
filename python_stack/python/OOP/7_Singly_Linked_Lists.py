45a900d19dbb8a37a12c9bc240b14d64c9321b27
Jacedeuce <jdhanna@gmail.com>
2019-06-04 18:17:53 -0700

    SLL # 2

--- python_stack/python/OOP/7_Singly_Linked_Lists.py
+++ python_stack/python/OOP/7_Singly_Linked_Lists.py
@@ -19,17 +19,18 @@ class SList:
 
     def get_last_node(self):
         next_node = self.head
+        ## add length value to store list length
         while next_node != None:
             last_node = next_node
             next_node = next_node.next
-        return last_node
+        return last_node ## return length value
 
     def add_to_back(self, val):
         if self.head == None:
             self.add_to_front(val)
             return self
         new_node = SLNode(val)
-        last_node = self.get_last_node()
+        last_node = self.get_last_node() ## need to account for length value return in future
         last_node.next = new_node
         return self
 
@@ -56,8 +57,25 @@ class SList:
                 node = node.next
                 if node.next == None and node.value == val:
                     prev_node.next = None
-
         return self
+
+    def insert_at(self, val, pos):
+        new_node = SLNode(val)
+        node = self.head
+        if pos == 0: ## case for beginning
+            self.head = new_node
+            new_node.next = node
+            return self
+        next_node = node.next
+        for i in range(0, pos): # traverse list elements
+            if next_node == None:
+                node.next = new_node
+                return self
+            node, next_node = next_node, next_node.next
+        node.next = new_node
+        new_node.next = next_node
+        return self
+
 
 
 class SLNode:
@@ -67,13 +85,21 @@ class SLNode:
 
 singlelist = SList()
 singlelist.add_to_front(10).add_to_front(9).add_to_front(8).add_to_front(7).add_to_front(6).add_to_front(5).add_to_front(4).add_to_front(3).add_to_front(2).add_to_front(1)
-# singlelist.print_values()
-singlelist.add_to_back(11)
-singlelist.remove_from_front()
+
+
+singlelist.insert_at(-1, 0)
+singlelist.insert_at(100, 45)
+singlelist.insert_at(99, 5)
 singlelist.print_values()
 print()
-singlelist.remove_from_back()
-singlelist.print_values()
+
+# # singlelist.print_values()
+# singlelist.add_to_back(11)
+# singlelist.remove_from_front()
+# singlelist.print_values()
+# print()
+# singlelist.remove_from_back()
+# singlelist.print_values()
 
 # singlelist.remove_val(7)
 # singlelist.print_values()
