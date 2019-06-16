d755a2d88bf9f4aaa68a8f04ed9f7aae3725703c
Jacedeuce <jdhanna@gmail.com>
2019-06-16 15:47:25 -0700

    refactor to fix Number game (Flask)

--- python_stack/flask/flask_fundamentals/8_Great_Number_Game/gn_game.py
+++ python_stack/flask/flask_fundamentals/8_Great_Number_Game/gn_game.py
@@ -17,14 +17,16 @@ def index():
     session['num'] = 3 #randint(0, 100)
     if 'leaderboard' not in session:
         session['leaderboard'] = []
+    leader_list = session['leaderboard']
     return render_template("index.html", num = session['num'], 
-                                        guesses_left = guesses_left)
+                                        guesses_left = guesses_left, leader_list=leader_list)
 
 @app.route("/number_guess", methods = ['POST'])
 def number_guess():
     guesses_left = 4-len(session['guessed_nums'])
     session['guess'] = request.form['form-guess']
     session['total_guesses'] = len(session['guessed_nums'])
+    print(session['total_guesses'])
     try:
         session['guess_value'] = int(session['guess'])
     except: 
@@ -35,28 +37,28 @@ def number_guess():
         return render_template("index.html", message = out_of_bounds)
     elif session['num'] == session['guess_value']:
         session['guessed_nums'].append(session['guess_value'])
+        session['total_guesses'] = len(session['guessed_nums'])
+        print(session['guessed_nums'])
         return render_template("restart.html", message = winner,
                                                 total_guesses=session['total_guesses'])
     else: 
         session['guessed_nums'].append(session['guess_value'])
+        session['total_guesses'] = len(session['guessed_nums'])
         if len(session['guessed_nums']) > 4:
             return render_template("restart.html", message = loser,
                                                 total_guesses=session['total_guesses'])
         elif session['guess_value'] > session['num']:
             direction = "lower"
             relation = "high"
-            return render_template("index.html", message = guess_again,
-                                                direction = direction,
-                                                relation = relation,
-                                                guesses_left = guesses_left)
         elif session['guess_value'] < session['num']:
             direction = "higher"
             relation = "low"
-            return render_template("index.html", message = guess_again,
-                                                direction = direction,
-                                                relation = relation,
-                                                guesses_left = guesses_left)
-        # return render_template("index.html", message = guess_again)
+        leader_list = session['leaderboard']
+        return render_template("index.html", message = guess_again,
+                                            direction = direction,
+                                            relation = relation,
+                                            guesses_left = guesses_left,
+                                            leader_list = leader_list)
 
 @app.route("/win_name", methods = ['POST'])
 def win_name():
@@ -64,16 +66,27 @@ def win_name():
     name = request.form['winner_name']
     guess = session['total_guesses']
     new_leader = createLeader(name, guess)
-    session['leaderboard'].append(new_leader)
+    
+    if 'leaderboard' in session:
+        leader_list = session['leaderboard']
+        leader_list.append(new_leader)
+    else:
+        leader_list = list(new_leader)
+
+    session['leaderboard'] = leader_list
     print(session)
-    return redirect("/leaderboard")
+    return redirect("/")
 
 @app.route("/leaderboard")
 def leaderboard():
     print(session)
-    leader_dict = []
-    leader_dict = session['leaderboard']
-    return render_template("leaderboard.html", leader_dict = leader_dict)
+    leader_list = session['leaderboard']
+    return render_template("leaderboard.html", leader_list = leader_list)
+
+@app.route("/clear")
+def clear():
+    session.clear()
+    return redirect('/')
 
 def createLeader(name, guess):
     return {
