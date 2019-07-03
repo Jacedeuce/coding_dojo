19806700d6c35e5cdb13a26bc13691a93b785c7f
Jason <jason@localhost.localdomain>
2019-07-02 21:57:55 -0700

    Interacting (MEAN)

--- mean_stack/js/OOP/OOP_game_map.js
+++ mean_stack/js/OOP/OOP_game_map.js
@@ -1,14 +1,59 @@
-var eeyore = { character: "Eeyore"}
-var heffalumps = { character: "Heffalumps"}
-var kanga = { character: "Kanga"}
-var owl = { character: "Owl"}
-var christopher_robin = { character: "Christopher Robin"}
-var rabbit = { character: "Rabbit"}
-var gopher = { character: "Gopher"}
-var piglet = { character: "Piglet"}
-var pooh = { character: "Winnie the Pooh" }
-var bees = { character: "Kanga"}
-var tigger = { character: "Tigger" }
+var eeyore = { character: "Eeyore",
+                            greet: function() {
+                                    console.log("EEEEEYore.....");
+                                    },
+};
+var heffalumps = { character: "Heffalumps",
+                            greet: function() {
+                                    console.log("Very good honey this, I don't know when I've tasted better!" );
+                                    },
+};
+var kanga = { character: "Kanga",
+                            greet: function() {
+                                    console.log("Why, hello, Tigger, dear.");
+                                    },
+};
+var owl = { character: "Owl",
+                            greet: function() {
+                                    console.log("This reminds me of the time when...");
+                                    },
+};
+var christopher_robin = { character: "Christopher Robin",
+                            greet: function() {
+                                    console.log("Silly ol' bear!");
+                                    },
+};
+
+var rabbit = { character: "Rabbit",
+                            greet: function() {
+                                    console.log("Let's pretend it isn't and see what happens");
+                                    },
+};
+var gopher = { character: "Gopher",
+                            greet: function() {
+                                    console.log("I'm not in the book, you know.");
+                                    },
+};
+var piglet = { character: "Piglet",
+                            greet: function() {
+                                    console.log("It is hard to be brave, when you're only a very small animal.");
+                                    },
+};
+var pooh = { character: "Winnie the Pooh",
+                            greet: function() {
+                                    console.log("Oh, bother!");
+                                    },
+};
+var bees = { character: "Bees",
+                            greet: function() {
+                                    console.log("BZZZZZ!");
+                                    },
+};
+var tigger = { character: "Tigger",
+                            greet: function() {
+                                    console.log("Once in a while someone amazing comes along, and here I am!");
+                                    },
+};
 
 eeyore.east = heffalumps
 eeyore.south = kanga
