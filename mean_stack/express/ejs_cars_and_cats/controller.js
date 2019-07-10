66af76601cbe74a941f848d1c1b22a2ded94152d
Jason <jason@localhost.localdomain>
2019-07-10 13:32:43 -0700

    EJS Cat Data - add Cat show page to display individual cat data - (MEAN)

--- mean_stack/express/ejs_cars_and_cats/controller.js
+++ mean_stack/express/ejs_cars_and_cats/controller.js
@@ -1,15 +1,25 @@
+cat1 = {id : 1, name: 'Earbiter', age : 2, fav_food : ["ears", "Kibbles & Bits"]}
+cat2 = {id : 2, name: 'Soup', age : 7, fav_food : ["Chicken Noodle", "Pea Soup"]}
+cat3 = {id : 3, name: 'Walkee', age : 4, fav_food : ["bananas", "eucalyptus"]}
+cat4 = {id : 4, name: 'Little Ceasar', age : 5, fav_food : ["Pizza", "Pepperoni"]}
+
+cat_arr = [cat1, cat2, cat3, cat4]
+
 module.exports = {
     index : (req, res)=>{
-        context = {pages : ['cars', 'cats']}
+        var context = {pages : ['cars', 'cats']}
         res.render('index', context)
     },
     cars : (req, res)=>{
         res.render('cars')
     },
-    cats : (req, res)=>{
-        res.render('cats')
-    },
     newcar : (req, res)=>{
         res.render('newcar')
+    },
+    cats : (req, res)=>{
+        res.render('cats', {cats : cat_arr})
+    },
+    cat_show : (req, res)=>{
+        res.render('cat_show', {cat : cat_arr[req.params.catID-1]})
     }
 }
--- mean_stack/express/ejs_cars_and_cats/routes.js
+++ mean_stack/express/ejs_cars_and_cats/routes.js
@@ -3,6 +3,7 @@ const controller = require("./controller");
 module.exports = function(app){
     app.get('/', controller.index)
     app.get('/cars', controller.cars)
-    app.get('/cats', controller.cats)
     app.get('/cars/new', controller.newcar)
+    app.get('/cats', controller.cats)
+    app.get('/cats/:catID', controller.cat_show)
 }
